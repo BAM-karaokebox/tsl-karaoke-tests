14bbf45fcff569a78bd014e3f218fbe9f0419b31
Laurent Malvert <laurent.malvert@gmail.com>
2022-06-02 11:43:28 +0200

    TSL-174 prepare repository for TSL Karaoke tests

---
+++ README.md
@@ -0,0 +1,7 @@
+# The Songs Laboratory - TSL Karaoke Tests
+
+This is our repository of test scenarios for the TSL Karaoke app.
+
+Feel free to take a peek under the hood and check how our engineering
+team ensures everything runs smoothly at BAM and TSL.
+
---
+++ playwright.config.ts
@@ -0,0 +1,51 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ * See https://playwright.dev/docs/test-configuration.
+ */
+const config: PlaywrightTestConfig = {
+  testDir: './tests',
+  /* Maximum time one test can run for. */
+  timeout: 300000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     */
+    timeout: 15000,
+  },
+  headless: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /*
+   * We allow one retry to make the tests less prone to time-out issues,
+   * but no more (to avoid aggravating ongoing performance crunches).
+   */
+  retries: 1,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. */
+  reporter: 'html',
+  /* Shared settings for all the projects below. */
+  use: {
+    /* Maximum time each action such as `click()` can take. */
+    actionTimeout: 15000,
+
+    /* Collect trace when a test fail. Check at the end of the report */
+    trace: 'retain-on-failure',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+  ],
+};
+
+export default config;
